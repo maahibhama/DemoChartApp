01335b54881ba040c9f0ab43e18cff4a34e62d11
Mahendra Bhama <maahibhama@gmail.com>
2019-03-28 20:12:05 +0530

    Initial Commit

---
+++ DemoChartApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoChartApp
+//
+//  Created by Maahi Bhama  on 28/03/19.
+//  Copyright © 2019 Mahendra Bhama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DemoChartAppTests/DemoChartAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoChartAppTests.swift
+//  DemoChartAppTests
+//
+//  Created by Maahi Bhama  on 28/03/19.
+//  Copyright © 2019 Mahendra Bhama. All rights reserved.
+//
+
+import XCTest
+@testable import DemoChartApp
+
+class DemoChartAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
